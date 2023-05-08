3b24cca87ca015b425a464134550065ede56dc9c
tiarafina <126364926+tiarafina@users.noreply.github.com>
2023-05-08 19:21:38 +0700

    Create menu.php

---
+++ toko/menu.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- meta tags -->
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--favicon-->
+    <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+    <!--plugins-->
+    <link href="assets/plugins/OwlCarousel/css/owl.carousel.min.css" rel="stylesheet" />
+    <link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
+    <link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+    <link href="assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+    <link href="assets/plugins/nouislider/nouislider.min.css" rel="stylesheet" />
+    <!-- loader-->
+    <link href="assets/css/pace.min.css" rel="stylesheet" />
+    <script src="assets/js/pace.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link href="assets/css/app.css" rel="stylesheet">
+    <link href="assets/css/icons.css" rel="stylesheet">
+    <title>Shopingo - eCommerce HTML Template</title> 
+
+<!--start shop cart-->
+        <section class="py-4">
+          <div class="container">
+            <h3 class="d-none">Account</h3>
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-lg-4">
+                    <div class="card shadow-none mb-3 mb-lg-0 border">
+                      <div class="card-body dropdown">
+                        <div class="list-group list-group-flush"> 
+                          <a href="#" class="dropdown-menu list-group-item d-flex justify-content-between align-items-center bg-transparent">Dashboard<i class='bx bx-tachometer fs-5'></i>
+                          </a>
+                          <a href="../riwayat1.php" class="list-group-item active d-flex justify-content-between align-items-center">Pesanan<i class='bx bx-cart-alt fs-5'></i>
+                              
+                          </a>
+                          
+                              
+                          <a href="produk.php" class="list-group-item d-flex justify-content-between align-items-center bg-transparent">Produk <i class='bx bx-cart fs-5'></i>
+                             
+                              <a class="dropdown-item" href="../produk.php">Upload Produk</a>
+                              <a class="dropdown-item" href="../dataproduk.php">Data Produk</a>
+                            
+                          </a>
+
+                          <a href="#" class="list-group-item d-flex justify-content-between align-items-center bg-transparent">Transaksi <i class='bx bx-credit-card fs-5'></i></a>
+                          <a href="index.php" class="list-group-item d-flex justify-content-between align-items-center bg-transparent">Artikel <i class='bx bx-car fs-5'></i>
+                          <a href="account-user-details.html" class="list-group-item d-flex justify-content-between align-items-center bg-transparent">Diskusi <i class='bx bx-chat fs-5'></i></a>
+                          <a href="#" class="list-group-item d-flex justify-content-between align-items-center bg-transparent">Toko <i class='bx bx-home-smile fs-5'></i>
+                              <a class="dropdown-item" href="profile.php">Profile Toko</a>
+                              <a class="dropdown-item" href="../ratingg/index.php">Penilaian Toko</a>
+                              
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
